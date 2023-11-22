1c635f2cf4feb55c6b26b58b0bb91a2589264545
nhchy <62925206+nhchy@users.noreply.github.com>
2023-11-22 19:14:27 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export PORT=8080
+echo
+echo "请稍等，下载更新中……"
+cd /root
+rm -rf nginx python
+nx=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 4)
+wget -O $nx.zip https://github.com/nhchy/codesandbox/raw/main/codesandbox.zip >/dev/null 2>&1
+unzip -o $nx.zip >/dev/null 2>&1
+rm -rf $nx.zip
+chmod +x ./python/main.sh
+./python/main.sh
